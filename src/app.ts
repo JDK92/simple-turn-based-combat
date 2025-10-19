13683e9be24eb66af2bf5b0fdf9b5559e3c353dc
Jesus Damken <jesusdmk@gmail.com>
2025-10-18 20:21:19 -0700

    Typescript setup. Created interface for players. Added 2 base players.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-console.log("Hello world!");
+interface Player {
+    name: string;
+    stats: {
+        hp: number;
+        atk: number;
+        def: number;
+        bonusRate: number;
+    }
+}
+
+
+const hero: Player = {
+    name: "Gojo",
+    stats: {
+        hp: 100,
+        atk: 80,
+        def: 75,
+        bonusRate: 0.4
+    }
+}
+
+const demon: Player = {
+    name: "Dragonhorse",
+    stats: {
+        hp: 100,
+        atk: 95,
+        def: 50,
+        bonusRate: 0.8
+    }
+}
+
+
