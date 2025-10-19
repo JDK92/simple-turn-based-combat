f56a0ab39928754b298286bd6ae822158faf0a26
Jesus Damken <jesusdmk@gmail.com>
2025-10-18 21:51:47 -0700

    Added menu logic

--- dist/app.js
+++ dist/app.js
@@ -1,54 +1,43 @@
-import 'colors';
-
-import { createInterface } from 'readline';
-
-const rl = createInterface({
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("colors");
+const readline_1 = require("readline");
+const rl = (0, readline_1.createInterface)({
     input: process.stdin,
     output: process.stdout
 });
-
 const menu = {
     1: "Attack",
     2: "Defend",
     3: "Cure",
     4: "Status",
     0: "Exit",
-}
-
+};
 const displayMenu = () => {
-    console.log("\n===== SIMPLE TURNED BASE GAME =====\n".red);
+    console.log("===== SIMPLE TURNED BASE GAME =====".red);
     for (const [key, value] of Object.entries(menu)) {
-        console.log(`${key}. ${value}`)
+        console.log(`${key}. ${value}`);
     }
-}
-
-const askQuestion = (question: string) => {
+};
+const askQuestion = (question) => {
     return new Promise(res => {
         rl.question(question, res);
     });
-}
-
-
+};
 const handleMenu = async () => {
-    let isRunning: boolean = true;
-
+    let isRunning = true;
     while (isRunning) {
         displayMenu();
-
         const answer = await askQuestion("\nWhat do you want to do?\n");
-
         console.log({ answer });
-
         if (answer == 0) {
             isRunning = false;
         }
-
         if (isRunning) {
-            await askQuestion(`\nPress enter to continue...\n`);
+            await askQuestion(`\Press enter to continue...`);
         }
     }
-}
-
+};
 handleMenu().finally(() => {
     rl.close();
 });
